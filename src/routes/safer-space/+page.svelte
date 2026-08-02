a00ddc1a422a5caee585759c48d11b2bd841250e
Rolle Laukkarinen <roni@dude.fi>
2026-08-02 17:30:51 +0300

    Add safer space principles page with animated rainbow heading

---
+++ src/routes/safer-space/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<h1 class="rainbow">{m.safer_heading()}</h1>
+
+<p>{m.safer_intro()}</p>
+
+<ol class="principles">
+	<li>{m.safer_1()}</li>
+	<li>{m.safer_2()}</li>
+	<li>{m.safer_3()}</li>
+	<li>{m.safer_4()}</li>
+	<li>{m.safer_5()}</li>
+	<li>{m.safer_6()}</li>
+</ol>
+
+<p>{m.safer_report()}</p>
+
+<p class="muted small">{m.safer_draft_note()}</p>
